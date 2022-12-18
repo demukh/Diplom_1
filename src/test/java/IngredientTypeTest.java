468afdee234dd9bfa62997d20a30004cb0e88a68
demukh <demukhsasha@mail.ru>
2022-12-18 17:53:06 +0300

    add_tests

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import praktikum.IngredientType;
+import static org.junit.Assert.*;
+
+
+public class IngredientTypeTest {
+
+    @Test
+    public void shouldGetIngredientType() {
+        assertEquals(IngredientType.valueOf("SAUCE"), IngredientType.SAUCE);
+        assertEquals(IngredientType.valueOf("FILLING"), IngredientType.FILLING);
+    }
+}
---
+++ src/test/java/PriceTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+import static org.junit.Assert.*;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class PriceTest {
+
+    private final IngredientType sauce = SAUCE;
+    private final float bunPrice;
+    private final float ingredientPrice;
+    private final float expectedPrice;
+
+    public PriceTest(float bunPrice, float ingredientPrice, float expectedPrice) {
+        this.bunPrice = bunPrice;
+        this.ingredientPrice = ingredientPrice;
+        this.expectedPrice = expectedPrice;
+    }
+    @Parameterized.Parameters
+    public static Object[][] getBurgerData() {
+        return new Object[][]{
+                {0, 0, 0},
+                {0, 50, 50},
+                {80, 0, 160},
+                {100, 100, 300},
+                {3.87f, 5.55f, 13.29f},
+        };
+    }
+    @Test
+    public void shouldGetPrice() {
+        Burger burger = new Burger();
+        Bun bun = new Bun("test bun", bunPrice);
+        burger.setBuns(bun);
+        Ingredient ingredient = new Ingredient(sauce, "test filling", ingredientPrice);
+        burger.addIngredient(ingredient);
+        assertEquals(expectedPrice, burger.getPrice(), 0.0);
+    }
+}
